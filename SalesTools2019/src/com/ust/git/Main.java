d9daffc2cf86ec93ffbff195e5146120e828b250
ShruthiS123 <55632863+ShruthiS123@users.noreply.github.com>
2019-09-23 15:16:28 +0530

    initial commit

---
+++ SalesTools2019/src/com/ust/git/Main.java
@@ -0,0 +1,15 @@
+package com.ust.git;
+
+public class Main {
+	
+
+	public static void main(String[] args){	
+		SalesData objSalesData=new SalesData();
+		System.out.println("hello ust");
+		displayGreetings();
+		objSalesData.display();
+	}
+	private static void displayGreetings(){
+		System.out.println("welcome to git");
+	}
+}
